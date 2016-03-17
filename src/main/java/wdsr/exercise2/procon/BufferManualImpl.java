e8ffa55722c44638bfab72ccf7977475b340c819
MrVesper <j.papiniak@wp.pl>
2016-03-17 08:54:53 +0100

    3 cz ćwiczenia exercise 2
    zaimplementowano BufferManualImp oraz BufferQueueImpl

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -3,6 +3,7 @@ package wdsr.exercise2.procon;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,20 +13,37 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class BufferManualImpl implements Buffer {
 
-	Object lock1 = new Object();
-
+	Lock lock = new ReentrantLock();
+	Condition notFull = lock.newCondition();///jezeli lista jest pelna
+	Condition notEmpty = lock.newCondition();///jezeli lista pusta
+	
 	int maxAmountOfOrders = 10000;
 	List<Order> orderList = new ArrayList<Order>();
 
 	public void submitOrder(Order order) throws InterruptedException {	
-		synchronized (lock1) {
-			orderList.add(order);
+		lock.lock();
+		try {
+			while(orderList.size() == maxAmountOfOrders)
+			{
+				notFull.await();///producer czeka az sie zwolni miejsce na nowe zamowienia
+			}
+			orderList.add(order);///dodajemy zamowienie bo lista nie jest pelna
+								///oraz notFull otrzymal signal
+			notEmpty.signal();///Lista nie jest pusta wiec konsument moze odebrac zamowienie
+		} finally {
+			lock.unlock();
 		}
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		synchronized (lock1) {
+		lock.lock();
+		try {
+			while(orderList.size()==0)notEmpty.await();///Konsument czeka z odbiorem zamowienia bo lista zamowien jest pusta
+			if(orderList.size() < maxAmountOfOrders)notFull.signal();///Lista zamowien nie jest pusta wiec 
+			///konsument moze odebrac zamowienia a producent przyjac kolejne
 			return orderList.remove(0);
+		} finally {
+			lock.unlock();
 		}
 	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -8,7 +8,7 @@ import java.util.concurrent.BlockingQueue;
  * Task: implement Exchange interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
-	public static BlockingQueue <Order> queue = new ArrayBlockingQueue<Order>(200000);
+	public  BlockingQueue <Order> queue = new ArrayBlockingQueue<Order>(200000);
 	
 	public void submitOrder(Order order) throws InterruptedException {
 		queue.put(order);
